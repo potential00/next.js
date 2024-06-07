9b4d54167f36cef930daa6353052f3f4721e1595
potential00 <158043435+potential00@users.noreply.github.com>
2024-06-07 22:42:05 +0100

    Add files via upload

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+
+
+const AboutPage = () => {
+    return (
+        <div>
+            <div> About Arsenal Fc</div>
+        </div>
+
+    )
+}
+
+export default AboutPage;
---
+++ src/app/example/page.js
@@ -0,0 +1,20 @@
+const ExamplePage = () => {
+
+    const abc = {
+        display: 'flex',
+        padding: '60px',
+        backgroudcolor: 'blue'
+
+    }
+
+    return(
+        <div style={abc}>
+            <div>Number 1</div>
+            <div>Number 2</div>
+            <div>Number 3</div>
+        </div>
+    )
+    
+};
+
+export default ExamplePage;
